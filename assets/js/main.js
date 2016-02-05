359644a4086103df113de56582b366283f9ddc59
Jennifer Patel <jennifer@goinvo.com>
2016-02-05 18:18:36 -0500

    fixed timeline, fixed header, removed providers

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
 	console.log("ready");
 
+	$(window).bind('scroll', function() {
+	    var distance = 50;
+	    console.log("scroll");
+	    if ($(window).scrollTop() > distance) {
+	      $('nav').addClass('scrolled');
+	    }
+	    else {
+	      $('nav').removeClass('scrolled');
+	    }
+	  });
 
 });
